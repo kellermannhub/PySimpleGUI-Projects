8a8f9e533b4fb5f4386e9744b0a8a06d400277ea
Misael Oliveira <misaelmoliveira@gmail.com>
2022-04-07 16:40:48 -0300

    Small improvements

--- Projeto Inicial/main.py
+++ Projeto Inicial/main.py
@@ -1,14 +1,35 @@
 import PySimpleGUI as sg
 
 
+sg.theme("Darkgray10")
+
 layout = [
-        [sg.Text('Filename')],
-        [sg.Input(), sg.FileBrowse()],
-        [sg.Button('Ok'), sg.Button('Cancel')]
+        [sg.Text("Filename",)],
+        [sg.Input(key="-FILE-", text_color="black",
+         background_color="white"), sg.FileBrowse(key="-BROWSE-")],
+        [sg.Button("Ok", key="-BTNOK-"), sg.Button("Cancel")],
         ]  # Each line it's a line in the interface.
 
-window = sg.Window('First Project', layout)
+window = sg.Window('First Project', layout, size=(500, 400))
 
-event, values = window.read()
 
-window.close()  # From here we can already see a window and browse files.
+def update_input():
+    window["-FILE-"].update("")
+
+while True:
+    event, values = window.read()
+
+    # From here we can already see a window and browse files.
+    if event == sg.WIN_CLOSED or event == "Cancel":
+        print("Window closed.")
+        break
+
+    elif event == "-BTNOK-":
+        sg.popup(f"The chosen file was: {values['-FILE-']}")
+        update_input()  # Cleans the input after user clicks ok.
+
+    print("Event: ", event)
+    print("*"*40)
+
+
+window.close()
